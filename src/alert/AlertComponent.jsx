e847627dd86136a45fa21881b871e48ed60f174c
Ryan Edwards <ryan.deland.edwards@gmail.com>
2022-09-13 12:14:59 -0500

    Update AlertComponent to make Alert itself clickable if link present

--- src/alert/AlertComponent.jsx
+++ src/alert/AlertComponent.jsx
@@ -3,23 +3,35 @@ import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import Link from '@mui/material/Link';
 
-const AlertComponent = ({ text, link, timeLimit, id, alertTitle, alertType, removeAlert }) => (
-  <Snackbar
-    className='alert-component'
-    key={id}
-    open={true}
-    autoHideDuration={timeLimit}
-    onClose={(event, reason) => {
-      if (reason === 'clickaway') return;
-      removeAlert(id);
-    }}
-    anchorOrigin={{ vertical: 'top', horizontal: 'right'}}
-  >
+const AlertComponent = ({ text, link, timeLimit, id, alertTitle, alertType, removeAlert }) => {
+  const renderAlert = () => (
     <Alert severity={alertType} onClose={() => removeAlert(id)}>
       {alertTitle && <h4>{alertTitle}</h4>}
-      {link ? <Link underline='hover' href={link}>{text}</Link> : text }
-    </Alert> 
-  </Snackbar>
-)
+      {text}
+    </Alert>
+  );
+
+  return (
+    <Snackbar
+      className='alert-component'
+      key={id}
+      open={true}
+      autoHideDuration={timeLimit}
+      onClose={(event, reason) => {
+        if (reason === 'clickaway') return;
+        removeAlert(id);
+      }}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right'}}
+    >
+      {link ? (
+        <Link underline='none' href={link} target='_blank' rel='noopener'>
+          {renderAlert()}
+        </Link>
+      ) : (
+        renderAlert()
+      )}
+    </Snackbar>
+  )
+}
 
 export default AlertComponent;
